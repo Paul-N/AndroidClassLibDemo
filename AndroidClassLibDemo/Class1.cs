5cf59b460eddf0c8cd7be4a77ad2a3b69635d292
EvilGenius <evilgenius@211.ru>
2020-12-23 10:23:10 +0300

    Add project files.

---
+++ AndroidClassLibDemo/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace AndroidClassLibDemo
+{
+    public class Class1
+    {
+        public void TestMethod()
+        {
+            var type = typeof(global::MvvmCross.Platforms.Android.Presenters.MvxAndroidViewPresenter); //Where the type?
+        }
+    }
+}
